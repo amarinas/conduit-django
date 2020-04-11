7728bf2f00325b9084bf55c9ce25fa0cca1069e8
ali <ali.marinas@icloud.com>
2020-04-10 18:25:47 -0700

    first commit git push -u origin master git status

---
+++ conduit/apps/authentication/models.py
@@ -0,0 +1,10 @@
+import jwt
+
+from datetime import datetime, timedata
+
+from django.conf import settings
+from django.contrib.auth.models import {
+    AbstractBaseUser, BaseUserManager, PermissionsMixin
+}
+
+from django.db import models
