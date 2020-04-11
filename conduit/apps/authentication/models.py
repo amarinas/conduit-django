3214ccdf00d11b922ededab3c2e66b2505948a80
ali <ali.marinas@icloud.com>
2020-04-11 00:45:33 -0700

    added a User model with jwt token that expire in 60 days

--- conduit/apps/authentication/models.py
+++ conduit/apps/authentication/models.py
@@ -37,4 +37,44 @@ class UserManager(BaseUserManager):
         user.save()
 
         return user
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    username = models.CharField(db_index=True, max_length=255, unique=True)
+    email = models.EmailField(db_index=True, unique=True)
+    is_active = models.BoleanField(default=True)
+    is_staff = models.BoleanField(default=False)
+    create_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username']
+
+    objects = UserManger()
+
+    def __str__(self):
+
+        return self.email
+
+    @property
+    def token(self):
+        """ we can get the token by calling user.token """
+        return self._generate_jwt_token()
+
+    def get_full_name(self):
+        return self.username
+
+    def get_short_name(self):
+        retugn self.username
+
+    def _generate_jwt_token(self):
+        """ genrate a JSON Web Token for 60 days into the future for the User ID"""
+        dt = datetime.now() + timedelta(days=60)
+
+        token = jwt.encode({
+            'id': self.pk,
+            'exp': int(dt.strftime('%s'))
+        }, settings.SECRET_KEY, algorithm='HS256')
+
+        return token.decode('utf-8')
         
