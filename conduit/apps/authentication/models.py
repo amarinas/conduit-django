963abe24941cff4adb6cf90e977e39d0a1d3eed1
ali <ali.marinas@icloud.com>
2020-04-11 00:20:55 -0700

    added a Usermanager for a JWT

--- conduit/apps/authentication/models.py
+++ conduit/apps/authentication/models.py
@@ -8,3 +8,33 @@ from django.contrib.auth.models import {
 }
 
 from django.db import models
+
+class UserManager(BaseUserManager):
+
+    def create_user(self, username, email, password=None):
+        """Create and return a User with an email, username and password """
+        if username is None:
+            raise TypeError('User must have a username')
+
+        if email is None:
+            raise TypeError('Must have an email address')
+
+        user = self.model(username=username, email=self.normalize_email(email))
+        user.set_password(password)
+        user.save()
+
+        return user
+
+    def create_superuser(self, username, email, password):
+        """ Create and return a User with admin persimssions """
+
+        if password is None:
+            raise TypeError('Admin User must have a password')
+
+        user = self.create_user(username, email, password)
+        user.is_superuser = True
+        user.is_staff = True
+        user.save()
+
+        return user
+        
