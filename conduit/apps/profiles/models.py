8727b7401c606f569e28d4a7d4fff00fe462928f
ali <ali.marinas@icloud.com>
2020-04-13 12:46:17 -0700

    added models for profiles

--- conduit/apps/profiles/models.py
+++ conduit/apps/profiles/models.py
@@ -1,1 +1,17 @@
 from django.db import models
+
+from conduit.apps.core.models import TimestampedModel
+
+class Profile(TimestampedModel):
+    user = models.OnetoOneField(
+        'authentication.User', on_delete=models.CASCADE
+    )
+
+    bio = models.TextField(blank=True)
+
+    image = models.URLField(blank=True)
+
+
+
+    def __str__(self):
+        return self.user.username
