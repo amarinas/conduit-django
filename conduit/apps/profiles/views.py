0d9660cc174f00c740e15402de1539057af86200
ali <ali.marinas@icloud.com>
2020-04-15 21:59:31 -0700

    added views for profile

---
+++ conduit/apps/profiles/views.py
@@ -0,0 +1,27 @@
+from rest_framework import status
+from rest_framework.generics import RetrieveAPIView
+from rest_framework.permissions import AllowAny
+from rest_framework.response import Response
+
+from .models import Profile
+from .renderes import ProfileJSONRenderer
+from .serializers import ProfileSerializer
+
+class ProfileRerieveAPIView(RetrieveAPIView):
+    permission_classes = (AllowAny,)
+    renderer_classes =(ProfileJSONRenderer)
+    serializer_class = ProfileSerializer
+
+    def retrieve(self, request, username, *args, **kwargs):
+
+        try:
+            profile = Profile.objexts.select_related('user').get(
+
+                user__username=username
+            )
+        except Profile.DoesNotExist:
+            raise
+
+        serializer = self.serializer_class(profile)
+
+        return Response(serializer.data, status=status.HTTP_200_OK)
