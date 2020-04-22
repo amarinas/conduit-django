081877fdc255f4d2103965b0375de4d998f8e2c5
ali <ali.marinas@icloud.com>
2020-04-22 16:18:14 -0700

    updated profiles to recieve the follower and followee

--- conduit/apps/profiles/serializers.py
+++ conduit/apps/profiles/serializers.py
@@ -6,10 +6,26 @@ class ProfileSerializer(serializers.ModelSerializer):
     username = serializers.CharField(source= 'user.username')
     bio = serializers.CharField(allow_blank=True, required=False)
     image = serializers.SerializerMethodField()
+    following = serializers.SerializerMethodField()
 
+    def get_fofllowing(self, instance):
+        request = self.context.get('request', None)
+
+        if request is None:
+            return False
+
+        if not request.user.is_authenticated:
+            return False
+
+        follower = request.user.profile
+        followee = instance
+
+        return follower.is_following(followee)
+
+        
     class Meta:
         model = Profile
-        fields =('username', 'bio', 'image')
+        fields =('username', 'bio', 'image', 'following',)
         read_only_fields= ('username',)
 
     def get_image(self, obj):
--- conduit/apps/profiles/urls.py
+++ conduit/apps/profiles/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from .views import ProfileRerieveAPIView
+from .views import ProfileRerieveAPIView, ProfileFollowAPIView
 
 app_name = 'profiles'
 urlpatterns =[
-    path('profiles/<str:username>', ProfileRerieveAPIView.as_view())
+    path('profiles/<str:username>', ProfileRerieveAPIView.as_view()),
+    path('profiles/<str:username>/follow', ProfileFollowAPIView.as_view()),
 ]
