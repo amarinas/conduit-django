16d8941fdf1df6661b1e97890db141161cda1f36
ali <ali.marinas@icloud.com>
2020-04-26 15:44:29 -0700

    added filtering

--- conduit/apps/articles/urls.py
+++ conduit/apps/articles/urls.py
@@ -4,7 +4,7 @@ from rest_framework.routers import DefaultRouter
 
 from .views import (
     ArticleViewSet, CommentsListCreateAPIView, CommentsDestroyAPIView,
-    ArticleFavoriteAPIView, TagListAPIView
+    ArticleFavoriteAPIView, TagListAPIView, ArticlesFeedAPIView
 )
 router = DefaultRouter(trailing_slash=False)
 router.register(r'articles', ArticleViewSet)
@@ -18,4 +18,5 @@ urlpatterns=[
     path('aricles/<slug:article_slug>/comments/<int:comment_pk>', CommentsDestroyAPIView.as_view()),
     path('articles/<slug:article_slug>/favorite', ArticleFavoriteAPIView.as_view()),
     path('tags', TagListAPIView.as_view()),
+    path('articles/feed', ArticlesFeedAPIView.as_view()),
 ]
